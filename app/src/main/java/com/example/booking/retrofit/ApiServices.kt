bf12c5fa7d76021961336ed5b156a89a343ffd89
Denta Septyan <dentacoy@gmail.com>
2025-06-19 17:34:46 +0700

    adding booking api

--- app/src/main/java/com/example/booking/retrofit/ApiServices.kt
+++ app/src/main/java/com/example/booking/retrofit/ApiServices.kt
@@ -7,6 +7,7 @@ import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.GET
 
 interface ApiServices {
     @POST("register")
@@ -27,4 +28,9 @@ interface ApiServices {
         @Part paymentProof: MultipartBody.Part,
         @Part("notes") notes: String
     ): Call<BookingResponse>
+
+    @GET("bookings")
+    fun getBookings(
+        @Header("Authorization") token: String
+    ): Call<List<DataBooking>>
 }
